56084ed750821778f57381480421456dc52ba665
aman123424 <ch20b010@smail.iitm.ac.in>
2026-08-21 22:53:15 +0530

    single responsiblity principle

---
+++ 02-solid_principles/single_responsibility_principle/SRP_followed.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Product
+{
+public:
+    string name;
+    int price;
+
+    Product(string name, int price)
+    {
+        this->name = name;
+        this->price = price;
+    }
+};
+
+// Only responsible for the cart related business logic
+class ShoppingCart
+{
+private:
+    vector<Product *> products;
+
+public:
+    void addProduct(Product *product)
+    {
+        products.push_back(product);
+    }
+
+    const vector<Product *> &getProducts()
+    {
+        return products;
+    }
+
+    // 1. Calculates the total price of the products in the cart
+    int calculateTotalPrice()
+    {
+        int total = 0;
+        for (auto product : products)
+        {
+            total += product->price;
+        }
+        return total;
+    }
+};
+
+// Responsible for printing invoice
+class CartInvoicePrinter
+{
+private:
+    ShoppingCart *cart;
+
+public:
+    CartInvoicePrinter(ShoppingCart *cart)
+    {
+        this->cart = cart;
+    }
+
+    void printInvoice()
+    {
+        cout << "Shopping Cart Invoice" << endl;
+        vector<Product *> products = cart->getProducts();
+        for (auto p : products)
+        {
+            cout << p->name << ": $" << p->price << endl;
+        }
+        cout << "Total Amount: $" << cart->calculateTotalPrice() << endl;
+    }
+};
+
+// Responsilble for saving to database
+class SaveCartToDB
+{
+    private:
+        ShoppingCart* cart;
+
+    public:
+        SaveCartToDB(ShoppingCart* cart) {
+            this->cart = cart;
+        }
+
+        void saveToDB()
+        {
+            cout << "Saving Shopping Cart to database..." << endl;
+        }
+};
+
+int main()
+{
+    ShoppingCart *cart = new ShoppingCart();
+
+    cart->addProduct(new Product("Laptop", 93700));
+    cart->addProduct(new Product("Laptop Stand", 500));
+    cart->addProduct(new Product("Pen Drive", 1250));
+
+    CartInvoicePrinter *printer = new CartInvoicePrinter(cart);
+    printer->printInvoice();
+}
---
+++ 02-solid_principles/solid_principles.cpp
@@ -0,0 +1,6 @@
+// SOLID Principles
+// S: Single Responsibility Principle (SRP) : A class should have only one reason to change, it should do only one thing and not multiple responsibilities.
+// O: Open-Close Principle (OCP) : 
+// L: Liskov Substitution Principle (LSP)
+// I: Interface Segregation Principle (ISP)
+// D: Dependency Inversion Principle (DIP)
