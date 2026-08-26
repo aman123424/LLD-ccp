360d04a3a1922ad662ce2a553c47e8e07881d7ab
aman123424 <ch20b010@smail.iitm.ac.in>
2026-08-27 02:42:01 +0530

    solid principles concluded

--- 02-solid_principles/solid_principles.cpp
+++ 02-solid_principles/solid_principles.cpp
@@ -2,5 +2,5 @@
 // S: Single Responsibility Principle (SRP) : A class should have only one reason to change, it should do only one thing and not multiple responsibilities.
 // O: Open-Close Principle (OCP) : A class should be open for extention but closed for modification. We must not change the old code to add new feature.
 // L: Liskov Substitution Principle (LSP) : Subclasses should be subsitutable for their base classes.
-// I: Interface Segregation Principle (ISP)
-// D: Dependency Inversion Principle (DIP)
+// I: Interface Segregation Principle (ISP) : Many client specific interfaces are better than one general purpose interface. The client should not be forced to implement the methods they don't need.
+// D: Dependency Inversion Principle (DIP) : High Level module should not depend on the low level module, but rather both should depend on the interface module.
